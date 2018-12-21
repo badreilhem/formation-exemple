60386437693b6ef24f8b0308333ce0615298265f
Ilhem Badreddine <ilhem.badreddine@pressinnov.com>
2018-12-21 10:54:27 +0100

    git init

---
+++ src/test/java/com/example/formationexemple/FormationExempleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.formationexemple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormationExempleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
